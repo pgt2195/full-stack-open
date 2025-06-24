861b2465566763980a064be72fd646b4a8b35ed7
Paul Grossetête <paulgrossetete.code@gmail.com>
2025-06-24 21:00:35 +0200

    Terminé l'exercice 3.9

--- part2/phonebook/src/services/persons.jsx
+++ part2/phonebook/src/services/persons.jsx
@@ -1,25 +1,25 @@
-import axios from 'axios'
-const baseUrl = 'http://192.168.1.31:3002/persons' // Works for my configuration but needs to be changed to localhost if needed
+import axios from "axios";
+const baseUrl = "http://192.168.1.31:3001/api/persons"; // Works for my configuration but needs to be changed to localhost if needed
 // const baseUrl = 'http://localhost:3002/persons'
 
 const getAll = () => {
-  const request = axios.get(baseUrl)
-  return request.then(response => response.data)
-}
+  const request = axios.get(baseUrl);
+  return request.then((response) => response.data);
+};
 
-const create = newObject => {
-  const request = axios.post(baseUrl, newObject)
-  return request.then(response => response.data)
-}
+const create = (newObject) => {
+  const request = axios.post(baseUrl, newObject);
+  return request.then((response) => response.data);
+};
 
-const deleteEntry = id => {
-  const request = axios.delete(`${baseUrl}/${id}`)
-  return request.then(response => response.data)
-}
+const deleteEntry = (id) => {
+  const request = axios.delete(`${baseUrl}/${id}`);
+  return request.then((response) => response.data)
+};
 
 const update = (id, newObject) => {
-  const request = axios.put(`${baseUrl}/${id}`, newObject)
-  return request.then(response => response.data)
-}
+  const request = axios.put(`${baseUrl}/${id}`, newObject);
+  return request.then((response) => response.data);
+};
 
-export default { getAll, create, deleteEntry, update }
+export default { getAll, create, deleteEntry, update };
